f6e0f263a335841db81ca56040c01bf6711efc3d
Hector Brown <hectorjbrown@gmail.com>
2020-03-04 23:39:07 +0000

    Add project files.

---
+++ HoneyCombTree/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace HoneyCombTree
+{
+    partial class FRM_main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PB_main = new System.Windows.Forms.PictureBox();
+            this.TIM_main = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.PB_main)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PB_main
+            // 
+            this.PB_main.BackColor = System.Drawing.Color.Black;
+            this.PB_main.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PB_main.Location = new System.Drawing.Point(0, 0);
+            this.PB_main.Name = "PB_main";
+            this.PB_main.Size = new System.Drawing.Size(800, 450);
+            this.PB_main.TabIndex = 0;
+            this.PB_main.TabStop = false;
+            this.PB_main.Paint += new System.Windows.Forms.PaintEventHandler(this.PB_main_Paint);
+            // 
+            // TIM_main
+            // 
+            this.TIM_main.Enabled = true;
+            this.TIM_main.Interval = 10;
+            this.TIM_main.Tick += new System.EventHandler(this.TIM_main_Tick);
+            // 
+            // FRM_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PB_main);
+            this.Name = "FRM_main";
+            this.Text = "Honeycomb Tree";
+            this.Load += new System.EventHandler(this.FRM_main_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FRM_main_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.PB_main)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PB_main;
+        private System.Windows.Forms.Timer TIM_main;
+    }
+}
+
---
+++ HoneyCombTree/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HoneyCombTree
+{
+    public partial class FRM_main : Form
+    {
+        private readonly Color CURSORCOLOR = Color.Green;
+        private readonly int CURSORRAD = 5, CURSORTHICKNESS = 2;
+        private readonly int STALKWIDTH = 2;
+
+        private List<Growth> growths;
+        private Growth selected;
+        private Point center;
+        private List<Tuple<PointF, PointF, Color>> persistentLines;
+        public FRM_main()
+        {
+            InitializeComponent();
+        }
+
+        private void FRM_main_Load(object sender, EventArgs e)
+        {
+            center = new Point(PB_main.Width / 2, PB_main.Height / 2);
+            growths = new List<Growth>();
+            //initial state
+            growths.Add(new Growth(center, Growth.Direction.Up));
+            growths.Add(new Growth(center, Growth.Direction.Down));
+            selected = growths[1];
+            persistentLines = new List<Tuple<PointF, PointF, Color>>();
+        }
+
+        private void TIM_main_Tick(object sender, EventArgs e)
+        {
+            foreach (Growth growth in growths)
+            {
+                growth.Step();
+            }
+            PB_main.Refresh();
+        }
+
+        private void FRM_main_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Tab)
+            {
+                RotateSelection();
+            }
+            else if (e.KeyCode == Keys.Space)
+            {
+                Split();                
+            }
+        }
+
+        private void Split()
+        {
+            growths.Add(new Growth(selected, true));
+            growths.Add(new Growth(selected, false));
+            persistentLines.Add(new Tuple<PointF, PointF, Color>(selected.Base, selected.Position, selected.Color));
+            growths.Remove(selected);
+            selected = growths[growths.Count - 1];
+        }
+        private void RotateSelection()
+        {
+            int index = -2;
+            for (int i = 0; i < growths.Count; i++)
+            {
+                if (growths[i] == selected)
+                {
+                    index = i;
+                }
+            }
+            index++;
+            if (index == growths.Count)
+            {
+                index = 0;
+            }
+            selected = growths[index];
+        }
+        private void PB_main_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawEllipse(new Pen(new SolidBrush(CURSORCOLOR), CURSORTHICKNESS), 
+                new Rectangle(Convert.ToInt32(selected.Position.X) - CURSORRAD, 
+                Convert.ToInt32(selected.Position.Y) - CURSORRAD, 
+                CURSORRAD * 2, CURSORRAD * 2));
+            foreach (Growth growth in growths)
+            {
+                g.DrawLine(new Pen(new SolidBrush(growth.Color), STALKWIDTH), growth.Base, growth.Position);
+            }
+            foreach (Tuple<PointF, PointF, Color> line in persistentLines)
+            {
+                g.DrawLine(new Pen(new SolidBrush(line.Item3), STALKWIDTH), line.Item1, line.Item2);
+            }
+        }
+    }
+    public class Growth
+    {
+        public Direction MovementDirection;
+        public PointF Position;
+        public PointF Base;
+        public Color Color;
+        private readonly float GROWTHRATE = 0.1f;
+        public enum Direction { Up, UpRight, Right, DownRight, Down, DownLeft, Left, UpLeft }
+        public Growth(Growth parent, bool leftwards)
+        {
+            MovementDirection = NextDirection(parent.MovementDirection, leftwards);
+            Position = parent.Position; Base = parent.Position;
+            Color = Color.LimeGreen;
+        }
+        public Growth(Point _position, Direction _movementDirection)
+        {
+            Position = _position; MovementDirection = _movementDirection; Base = _position;
+            Color = Color.LimeGreen;
+        }
+        private Direction NextDirection(Direction input, bool leftwards)
+        {
+            if (leftwards)
+            {
+                switch (input)
+                {
+                    case Direction.Up:
+                        return Direction.UpLeft;
+                    case Direction.UpLeft:
+                        return Direction.Left;
+                    case Direction.Left:
+                        return Direction.DownLeft;
+                    case Direction.DownLeft:
+                        return Direction.Down;
+                    case Direction.Down:
+                        return Direction.DownRight;
+                    case Direction.DownRight:
+                        return Direction.Right;
+                    case Direction.Right:
+                        return Direction.UpRight;
+                    case Direction.UpRight:
+                        return Direction.Up;
+                }
+            }
+            else
+            {
+                switch (input)
+                {
+                    case Direction.Up:
+                        return Direction.UpRight;
+                    case Direction.UpRight:
+                        return Direction.Right;
+                    case Direction.Right:
+                        return Direction.DownRight;
+                    case Direction.DownRight:
+                        return Direction.Down;
+                    case Direction.Down:
+                        return Direction.DownLeft;
+                    case Direction.DownLeft:
+                        return Direction.Left;
+                    case Direction.Left:
+                        return Direction.UpLeft;
+                    case Direction.UpLeft:
+                        return Direction.Up;
+                }
+            }
+            //this line should never be reached
+            return Direction.Up;
+        }
+        public void Step()
+        {
+            switch (MovementDirection)
+            {
+                case Direction.Up:
+                    Position.Y -= GROWTHRATE;
+                    break;
+                case Direction.UpRight:
+                    Position.X += GROWTHRATE * Convert.ToSingle(Math.Pow(2, -0.5f));
+                    Position.Y -= GROWTHRATE * Convert.ToSingle(Math.Pow(2, -0.5f));
+                    break;
+                case Direction.Right:
+                    Position.X += GROWTHRATE;
+                    break;
+                case Direction.DownRight:
+                    Position.X += GROWTHRATE * Convert.ToSingle(Math.Pow(2, -0.5f));
+                    Position.Y += GROWTHRATE * Convert.ToSingle(Math.Pow(2, -0.5f));
+                    break;
+                case Direction.Down:
+                    Position.Y += GROWTHRATE;
+                    break;
+                case Direction.DownLeft:
+                    Position.X -= GROWTHRATE * Convert.ToSingle(Math.Pow(2, -0.5f));
+                    Position.Y += GROWTHRATE * Convert.ToSingle(Math.Pow(2, -0.5f));
+                    break;
+                case Direction.Left:
+                    Position.X -= GROWTHRATE;
+                    break;
+                case Direction.UpLeft:
+                    Position.X -= GROWTHRATE * Convert.ToSingle(Math.Pow(2, -0.5f));
+                    Position.Y -= GROWTHRATE * Convert.ToSingle(Math.Pow(2, -0.5f));
+                    break;
+            }
+        }
+    }
+}
